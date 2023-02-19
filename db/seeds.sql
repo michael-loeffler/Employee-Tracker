f97e19472a59af96e8853c4363def87c15944992
mloeff <michaelloeffler23@gmail.com>
2023-02-19 17:29:22 -0600

    adds all inquirer questions, if/else tree for menu options, creates Query class and shifts db connection to that file, as well as all 3 'view' queries

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO departments (department_name)
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Human Resources"),
+       ("Legal"),
+       ("Sales");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 1),
+       ("Engineering Team Lead", 180000, 1),
+       ("Accountant", 100000, 2),
+       ("Chief Finance Officer", 250000, 2),
+       ("Payroll Specialist", 80000, 3),
+       ("Onboarding Specialist", 80000, 3),
+       ("Director of Human Resources", 130000, 4),
+       ("Lawyer", 180000, 4),
+       ("General Counsel", 250000, 4),
+       ("Sales Associate", 80000, 5),
+       ("Sales Manager", 130000, 5);
+       
--- index.js
+++ index.js
@@ -1,13 +1,124 @@
-const mysql = require('mysql2');
+//-- IMPORT NECESSARY NODE PACKAGES AND MODULES --//
 const inquirer = require('inquirer');
+const Query = require('./lib/Query');
+const query = new Query();
 
-const db = mysql.createConnection(
+//-- INQUIRER PROMPTS, QUESTION ARRAYS FOR EACH ACTION AND 1 FOR ACTION OPTIONS --//
+const actionOptions = [
     {
-        host: 'localhost',
-        user: 'root',
-        password: '2363MySQL!8747',
-        database: 'company_db'
-    },
-    console.log(`Connected to the company_db database.`)
-);
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'action',
+        choices: [
+            `View all departments`, 
+            `View all roles`, 
+            `View all employees`, 
+            `Add a department`, 
+            `Add a role`, 
+            `Add an employee`, 
+            `Update an employee's role`, 
+        ],
+    }
+];
 
+const departmentQuestions = [
+    {
+        type: 'input',
+        message: `What is the name of the department?`,
+        name: 'department_name',
+    }
+];
+
+const roleQuestions = [
+    {
+        type: 'input',
+        message: `What is the name of the role?`,
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: `What is the salary of the role?`,
+        name: 'salary',
+    },
+    // {
+    //     type: 'list',
+    //     message: `Which department does the role belong to?`,
+    //     name: 'department_id',
+    //     choices: []
+    // }
+];
+
+const employeeQuestions = [
+    {
+        type: 'input',
+        message: `What is the employee's first name?`,
+        name: 'first_name',
+    },
+    {
+        type: 'input',
+        message: `What is the employee's last name?`,
+        name: 'last_name',
+    },
+    // {
+    //     type: 'list',
+    //     message: `What is the employee's role?`,
+    //     name: 'role_id',
+    //     choices: []
+    // },
+    // {
+    //     type: 'list',
+    //     message: `Who is the employee's manager?`,
+    //     name: 'manager_id',
+    //     choices: []
+    // }
+];
+
+const updateQuestions = [
+    // {
+    //     type: 'list',
+    //     message: `Which employee's role would you like to update?`,
+    //     name: 'employee_id',
+    //     choices: []
+    // },
+    // {
+    //     type: 'list',
+    //     message: `Which role would you like to assign to the selected employee?`,
+    //     name: 'role_id',
+    //     choices: []
+    // }
+]
+
+//-- FUNCTIONS --//
+//- Creates a function that initializes the app; it is called immediately when the app is ran and asks what action the user would like to take. -//
+function init() {
+    displayMenuOptions(actionOptions);
+};
+
+function displayMenuOptions(questions) {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            if (data.action === `View all departments`) {
+                query.displayDepartments();
+            } else if (data.action === `View all roles`) {
+                query.displayRoles();
+            } else if (data.action === `View all employees`) {
+                query.displayEmployees();
+            } else if (data.action === `Add a department`) {
+                
+            } else if (data.action === `Add a role`) {
+                
+            } else if (data.action === `Add an employee`) {
+                
+            } else if (data.action === `Update an employee's role`) {
+                
+            }
+        })
+};
+
+// function viewDepartments() {
+//     query.displayDepartments();
+//     displayMenuOptions(actionOptions); DID NOT WORK
+// }
+
+init();
